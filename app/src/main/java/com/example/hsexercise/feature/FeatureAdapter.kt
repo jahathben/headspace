3d3ae70798cd1a079729610500e4579cb91c4b56
jahathben <jahathben@gmail.com>
2020-02-10 00:25:09 -0800

    fleshing out adapter & layout & swapping url for download_url so glide can load successfully

--- app/src/main/java/com/example/hsexercise/feature/FeatureAdapter.kt
+++ app/src/main/java/com/example/hsexercise/feature/FeatureAdapter.kt
@@ -1,20 +1,45 @@
 package com.example.hsexercise.feature
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.hsexercise.R
 import com.example.hsexercise.feature.database.FeatureModel
+import kotlinx.android.synthetic.main.feature_item.view.*
 
-class FeatureAdapter(it: List<FeatureModel>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class FeatureAdapter(private var features: List<FeatureModel>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return FeatureViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.feature_item,
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        (holder as FeatureViewHolder).bind(features[position])
     }
 
+    override fun getItemCount(): Int {
+        return features.size
+    }
+
+    class FeatureViewHolder (private var view: View) : RecyclerView.ViewHolder(view) {
+        private val featureImage = view.feature_image
+        private val featureAuthor = view.feature_author
+        private val featureDimensions = view.feature_dimensions
+
+        fun bind(feature : FeatureModel) {
+            featureAuthor.text = feature.author
+            featureDimensions.text = view.context
+                .getString(R.string.feature_desc, feature.width.toString(), feature.height.toString())
+            Glide.with(view).load(feature.download_url).into(featureImage)
+        }
+    }
 }
+
