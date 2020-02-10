9d007dd56e7df7404f8c8975381320712686881c
jahathben <jahathben@gmail.com>
2020-02-10 00:18:50 -0800

    wiring up activity to view model to repository. fleshing out UI components and stock recycler view adapter

--- app/src/main/java/com/example/hsexercise/feature/FeatureActivity.kt
+++ app/src/main/java/com/example/hsexercise/feature/FeatureActivity.kt
@@ -1,8 +1,17 @@
 package com.example.hsexercise.feature
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.hsexercise.R
+import com.example.hsexercise.api.Resource
 import com.example.hsexercise.common.BaseActivity
+import com.example.hsexercise.feature.database.FeatureModel
+import com.google.android.material.snackbar.Snackbar
 
 class FeatureActivity : BaseActivity<FeatureViewModel>() {
     override val viewModelClass = FeatureViewModel::class.java
@@ -11,6 +20,34 @@ class FeatureActivity : BaseActivity<FeatureViewModel>() {
     override fun provideViewModelFactory() = FeatureViewModel.Factory()
 
     override fun onViewLoad(savedInstanceState: Bundle?) {
-        // todo: write code here
+        val recyclerView: RecyclerView = findViewById(R.id.feature_recycler)
+        val emptyView: TextView = findViewById(R.id.empty_view)
+        val loadingView: ProgressBar = findViewById(R.id.progressBar)
+
+        recyclerView.layoutManager = LinearLayoutManager(this,
+            RecyclerView.VERTICAL, false)
+        recyclerView.setHasFixedSize(true)
+
+        viewModel.getFeaturesList(application)
+
+        val booksObserver = Observer<Resource<List<FeatureModel>>> { list ->
+            when (list) {
+                is Resource.Success -> {
+                    loadingView.visibility = View.GONE
+                    emptyView.visibility = View.GONE
+                    list.data?.let { recyclerView.adapter = FeatureAdapter(it) }
+                }
+
+                is Resource.Loading -> {
+                    loadingView.visibility = View.VISIBLE
+                    emptyView.visibility = View.GONE
+                }
+
+                is Resource.Error -> Snackbar.make(recyclerView, getString(R.string.error_text),
+                    Snackbar.LENGTH_SHORT).show()
+            }
+        }
+
+        viewModel.featuresList.observe(this, booksObserver)
     }
 }
--- app/src/main/java/com/example/hsexercise/feature/FeatureViewModel.kt
+++ app/src/main/java/com/example/hsexercise/feature/FeatureViewModel.kt
@@ -1,13 +1,21 @@
 package com.example.hsexercise.feature
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import android.app.Application
+import androidx.lifecycle.*
+import com.example.hsexercise.api.Resource
+import com.example.hsexercise.feature.database.FeatureModel
+import com.example.hsexercise.repository.FeaturesRepository
 
 class FeatureViewModel : ViewModel() {
+    var featuresList : LiveData<Resource<List<FeatureModel>>> = MutableLiveData()
 
     class Factory :
         ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>) = FeatureViewModel() as T
+    }
+
+    fun getFeaturesList(application: Application) {
+        featuresList = FeaturesRepository.getFeaturesList(application, viewModelScope)
     }
 }
